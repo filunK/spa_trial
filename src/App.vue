ab810693a480379dd113d009055d0748e7752f40
FilunK <yishikawa@koromotani.sakura.ne.jp>
2019-06-22 21:26:17 +0900

    INITIAL COMMIT

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-toolbar-title class="headline">
+        <router-link to="/">Home</router-link>
+        <router-link to="/About">About</router-link>
+      </v-toolbar-title>
+      <v-spacer />
+
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script lang = "ts">
+import {Vue, Component} from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+@Component
+export default class App extends Vue {}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-layout justify-center>
+        <v-img
+          :src="require('../assets/logo.png')"
+          class="my-3"
+          contain
+          height="50"
+        ></v-img>
+    </v-layout>
+    <HelloWorld />
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
